6268f975f38658bb32bb4c32686bb7d43cb06b88
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-07-21 15:10:44 -0400

    setting to local storage but not staying there

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -10,22 +10,43 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([])
 
   useEffect(() => {
-    filterHandle()
-  },[todos, status])
+    const filterHandle = () => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))
+          break
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))
+          break
+        default:
+          setFilteredTodos(todos)
+          break
+      }
+    }
 
-  const filterHandle = () => {
-    switch (status) {
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true))
-        break
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false))
-        break
-      default:
-        setFilteredTodos(todos)
-      break
+    filterHandle()
+    saveLocal()
+  }, [todos, status])
+
+  useEffect(() => {
+    getLocal()
+  }, [])
+
+  const saveLocal = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+
+  const getLocal = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]))
+    } else {
+      let localTodo = JSON.parse(localStorage.getItem('todos'))
+      console.log(localTodo)
+      setTodos(localTodo)
     }
   }
+
+
   return (
     <div className="App">
       <header>
@@ -37,12 +58,12 @@ function App() {
         setInputText={setInputText}
         inputText={inputText}
         setStatus={setStatus}
-        />
+      />
       <TodoList
         setTodos={setTodos}
-        todos={todos} 
-        filteredTodos={filteredTodos} 
-        />
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
